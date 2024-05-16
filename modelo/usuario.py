b775a1667c3be2b78eeca4094ed1513085425dec
GuillermoGarciaBlazquez <guillegarciablazquez@gmail.com>
2024-05-16 21:43:41 +0200

    Añadido el docstring de la clase serie.

--- modelo/usuario.py
+++ modelo/usuario.py
@@ -1,2 +1,11 @@
 class Usuario:
-    pass
+    """
+    Clase que representa a un usuario.
+
+    Cada usuario tiene asociado un correo electrónico y una lista de series y películas que ha visualizado.
+
+    Atributos:
+    · correo-electronico (str): Este es el correo electrónico del usuario
+    · peliculas-vistas (list of Pelicula) : La lista de películas que el usuario ha visualizado.
+    · series-vistas (list of Series): La lista de series que el usuario ha visualizado.
+    """
