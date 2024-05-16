317e38b7290c3745ac9bed0c71f6e1ceb16f6293
GuillermoGarciaBlazquez <guillegarciablazquez@gmail.com>
2024-05-16 21:46:46 +0200

    Añadido el docstring de la clase serie.

--- modelo/usuario.py
+++ modelo/usuario.py
@@ -5,7 +5,7 @@ class Usuario:
     Cada usuario tiene asociado un correo electrónico y una lista de series y películas que ha visualizado.
 
     Atributos:
-    · correo-electronico (str): Este es el correo electrónico del usuario
+    · correo-electronico (str): Este es el correo electrónico del usuario.
     · peliculas-vistas (list of Pelicula) : La lista de películas que el usuario ha visualizado.
     · series-vistas (list of Series): La lista de series que el usuario ha visualizado.
     """
