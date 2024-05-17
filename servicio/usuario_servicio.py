503aaaffe11bc423eb7e6881be6997e1bb184b18
carlospereez <cpa80@alu.ua.es>
2024-05-17 12:52:58 +0200

    comentada en docstring la clase usuario_servicio

--- servicio/usuario_servicio.py
+++ servicio/usuario_servicio.py
@@ -3,17 +3,25 @@ from typing import List
 import time
 import re
 from modelo.repositorio.usuario_repositorio import UsuarioRepositorio
+
 class UsuarioServicio:
     """
-    ...
+       Clase que representa el servicio de usuarios.
 
-    """
+       Con esta clase podremos gestionar los usuarios haciendo uso del repositorio. Incluye la posibilidad de crear nuevos usuarios,
+       la obtención de un usuario por su correo electrónico, la obtención de todos los usuarios y la
+       visualización de contenido multimedia por parte de un usuario.
+
+       Methods:
+       alta_usuario(correo_electronico: str) -> None: Coge el mail y crea un usuario
+       obtener_usuario(correo_electronico: str) -> Usuario: Devuelve un usuario por su correo electrónico.
+       obtener_usuarios() -> List[Usuario]: Devuelve una lista de todos los usuarios.
+       visualizar_media(usuario: Usuario, media: Media) -> None: Simulación de la visualización de una película o serie por parte de un usuario.
+       """
     @classmethod
     def alta_usuario(cls, correo_electronico: str) -> None:
         """
         ...
-        :param correo_electronico:
-        :return:
         """
         # Verificamos con una regex que el correo sea válido
         # En caso de que no sea válido, lanzamos una excepción
@@ -33,12 +41,8 @@ class UsuarioServicio:
     @classmethod
     def visualizar_media(cls, usuario: Usuario, media: Media):
         """
-
-        :param usuario:
-        :param media:
-        :return:
-        """
         ...
+        """
         time.sleep(10) #Simula que el usuario está viendo la peli/serie durante 10 segundos
         #funcion de la instancia de Media que hayamos recibido,
         #actualiza la lista de películas o series en el objeto y actualiza el usuario
