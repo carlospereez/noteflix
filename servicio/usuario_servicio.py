cbe8877f7e208be796e66445eee438bba6e31c7c
carlospereez <cpa80@alu.ua.es>
2024-05-17 12:31:59 +0200

    redefinidas las nuevas funciones para la clase UsuarioServicio

--- servicio/usuario_servicio.py
+++ servicio/usuario_servicio.py
@@ -1,14 +1,32 @@
-from modelo import Usuario, Media
+from modelo import Usuario, Media, Pelicula, Serie
+from typing import List
 class UsuarioServicio:
+    """
+    ...
+
+    """
     @classmethod
     def alta_usuario(cls, correo_electronico: str) -> None:
+        """
         ...
+        :param correo_electronico:
+        :return:
+        """
 
     @classmethod
-    def ver_media(cls, usuario: Usuario, media: Media) -> None:
-        ...
+    def obtener_usuario(cls, correo_electronico: str) -> Usuario:
+        return UsuarioRepositorio.obtener_usuario(correo_electronico)
 
     @classmethod
-    def comprobar_usuario(cls, correo_electronico: str) -> Usuario:
-        ...
+    def obtener_usuarios(cls) -> List[Usuario]:
+        return UsuarioRepositorio.obtener_usuarios()
 
+    @classmethod
+    def visualizar_media(cls, usuario: Usuario, media: Media):
+        """
+
+        :param usuario:
+        :param media:
+        :return:
+        """
+        ...
