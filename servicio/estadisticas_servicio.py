7daf8933baa300411895890e7aa51102a67409da
Joel-42-pilulin <jeb27@alu.ua.es>
2024-05-17 19:50:32 +0200

    modificada la función obtener_estadisticas() para que devuelva dos dataframes y dos diccionarios para mejor visualización

--- servicio/estadisticas_servicio.py
+++ servicio/estadisticas_servicio.py
@@ -1,8 +1,7 @@
 from modelo import Usuario
 from typing import Tuple, Dict, List # Hacemos import de las más comunes porque luego las necesitaremos
 class EstadisticasServicio:
-    def obtener_estadisticas(self, usuario: Usuario):
-        '''Por ahora esta función devuelve dos diccionarios pero creo que hace falta algo más'''
+    def obtener_estadisticas(cls, usuario: Usuario)-> Tuple[pd.DataFrame, pd.DataFrame, Dict[str, int], Dict[str, int]]:
         # Diccionarios para almacenar los recuentos de géneros de películas y series
         generos_peliculas: Dict[str, int] = {}
         generos_series: Dict[str, int] = {}
@@ -25,4 +24,29 @@ class EstadisticasServicio:
                 else:
                     generos_series[genero] += 1
 
-        return generos_peliculas, generos_series
+        # Diccionarios para almacenar los géneros y visualizaciones de películas y series
+        # para crear un DataFrame a partir de un diccionario con listas como valores
+        generos_peliculas_dict: Dict[str, list] = {}
+        generos_series_dict: Dict[str, list] = {}
+
+        # Pares clave-valor de los diccionarios
+        generos_peliculas_dict['Género'] = []
+        generos_peliculas_dict['Visualizaciones'] = []
+        generos_series_dict['Género'] = []
+        generos_series_dict['Visualizaciones'] = []
+
+        # Bucles para completar los diccionarios con los datos obtenidos
+        for genero, visualizaciones in generos_peliculas.items():
+            generos_peliculas_dict['Género'].append(genero)
+            generos_peliculas_dict['Visualizaciones'].append(visualizaciones)
+
+        for genero, visualizaciones in generos_series.items():
+            generos_series_dict['Género'].append(genero)
+            generos_series_dict['Visualizaciones'].append(visualizaciones)
+
+        # Creación de DataFrames a partir de los diccionarios anteriores
+        generos_peliculas_df: pd.DataFrame = pd.DataFrame(generos_peliculas_dict)
+        generos_series_df: pd.DataFrame = pd.DataFrame(generos_series_dict)
+
+        # Devuelve los DataFrames y los diccionarios
+        return generos_peliculas_df, generos_series_df, generos_peliculas, generos_series
