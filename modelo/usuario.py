757cb0d58be6587c984599e975767c4a3d9be737
GuillermoGarciaBlazquez <guillegarciablazquez@gmail.com>
2024-05-19 11:33:06 +0200

    Corregida la función __init__ de Usuario y añadidos sus respectivos parámetros.

--- modelo/usuario.py
+++ modelo/usuario.py
@@ -14,7 +14,7 @@ class Usuario:
     · series-vistas (list of Series): La lista de series que el usuario ha visualizado.
     """
 
-    def __init__(self, correo_electronico: str, peliculas_vistas: List[Pelicula], series_vistas, List[Serie]):
+    def __init__(self, correo_electronico: str, peliculas_vistas: List[Pelicula], series_vistas: List[Serie]):
         """
         Inicializa un objeto de la clase Usuario.
 
@@ -23,3 +23,6 @@ class Usuario:
         peliculas_vistas (list of Pelicula): La lista de películas que el usuario ha visto.
         series_vistas (list of Serie): La lista de series que el usuario ha visto.
         """
+        self.correo_electronico = correo_electronico
+        self.peliculas_vistas = peliculas_vistas
+        self.series_vistas = series_vistas
