8c129c040f2df2e03ce4b3ba63567eacdaa2ff52
Nicolas <nss66@alu.ua.es>
2024-05-22 19:23:45 +0200

    creada la clase media y hecho su docstring

--- modelo/media.py
+++ modelo/media.py
@@ -1,3 +1,16 @@
 from abc import ABC
 class Media(ABC):
-    pass
+    """
+    Clase abstracta que representa un contenido multimedia.
+
+    Esta clase sirve como base para otras clases que representan tipos específicos de contenido multimedia,
+    como películas o series. Cada contenido multimedia tiene un id, título, director, año y géneros.
+
+    Attributes:
+    id (int): El identificador único del contenido multimedia.
+    titulo (str): El título del contenido multimedia.
+    director (str): El director del contenido multimedia.
+    anio (int): El año de lanzamiento del contenido multimedia.
+    generos (list of str): La lista de géneros del contenido multimedia.
+    """
+
