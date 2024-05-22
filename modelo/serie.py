8f8cf173ae9fc4206bef7ffa3c5663a91ecd99c6
Nicolas <nss66@alu.ua.es>
2024-05-22 20:02:33 +0200

    creada la clase serie y hecho su docstring

--- modelo/serie.py
+++ modelo/serie.py
@@ -1,3 +1,16 @@
 from media import Media
 class Serie(Media):
-    pass
+    """
+        Clase que representa una serie.
+
+        Esta clase hereda de la clase Media y añade un atributo adicional: temporadas.
+        Cada serie tiene un id, título, director, año, géneros y número de temporadas.
+
+        Attributes:
+        id (int): El identificador único de la serie.
+        titulo (str): El título de la serie.
+        director (str): El director de la serie.
+        anio (int): El año de lanzamiento de la serie.
+        generos (list of str): La lista de géneros de la serie.
+        temporadas (int): El número de temporadas de la serie.
+        """
