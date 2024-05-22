b67ca184eaea1091ebf83a927b2b4c6816e74f7f
Nicolas <nss66@alu.ua.es>
2024-05-22 18:50:33 +0200

    comentada clase pelicula

--- modelo/pelicula.py
+++ modelo/pelicula.py
@@ -1,3 +1,18 @@
 from media import Media
 class Pelicula(Media):
+    """
+    Clase que representa una película.
+
+    Esta clase hereda de la clase Media y añade un atributo adicional: duracion.
+    Cada película tiene un id, título, director, año, géneros y duración.
+
+    Attributes:
+    id (int): El identificador único del pelicula.
+    titulo (str): El título de la película.
+    director (str): El director de la película.
+    anio (int): El año de lanzamiento de la película.
+    generos (list of str): La lista de géneros de la película.
+    duracion (int): La duración de la película en minutos.
+    """
+
     pass
