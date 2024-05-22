36e899a5fb6eb0bb1f0871da84b9d01ae02b3b6a
Nicolas <nss66@alu.ua.es>
2024-05-22 19:30:10 +0200

    creada la función init y hecho su docstring

--- modelo/media.py
+++ modelo/media.py
@@ -14,3 +14,21 @@ class Media(ABC):
     generos (list of str): La lista de géneros del contenido multimedia.
     """
 
+    def __init__(self, id, titulo, director, anio, generos):
+        """
+        Inicializa un objeto de la clase Media.
+
+        Parámetros:
+        id (int): El identificador único del contenido multimedia.
+        titulo (str): El título del contenido multimedia.
+        director (str): El director del contenido multimedia.
+        anio (int): El año de lanzamiento del contenido multimedia.
+        generos (list of str): La lista de géneros del contenido multimedia.
+        """
+        self.id = id
+        self.titulo = titulo
+        self.director = director
+        self.anio = anio
+        self.generos = generos
+
+
